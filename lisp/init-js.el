397fc5e6925938e8db3ff46eef71518a3d42ef1a
pritchet <pritchet@adobe.com>
2020-07-06 16:39:27 -0700

    Adding rjsx

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -7,4 +7,14 @@
   :config
   (setq js-indent-level 2))
 
+(use-package rjsx-mode
+  :ensure t
+  :config
+  (add-to-list 'magic-mode-alist '("\\(.\\|\n\\)*import React" . rjsx-mode))
+
+  (setq-local company-tooltip-align-annotations t)
+
+  (add-hook 'rjsx-mode-hook #'companhy-mode)
+  )
+
 (provide 'init-js)
